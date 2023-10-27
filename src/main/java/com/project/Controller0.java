53d419c832e3648aa71d3891689014be189a0699
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-10-27 16:14:22 +0200

    asda

--- src/main/java/com/project/Controller0.java
+++ src/main/java/com/project/Controller0.java
@@ -16,29 +16,25 @@ import javafx.scene.layout.AnchorPane;
 
 public class Controller0 {
     Random random = new Random();
-        
 
     @FXML
     private Button button0, boton1, boton2, boton3;
     @FXML
     private AnchorPane container;
     @FXML
-    private Label percentatge0, percentatge1 , percentatge2;
+    private Label percentatge0, percentatge1, percentatge2;
     @FXML
-    private ProgressBar progress0, progress1 , progress2;
+    private ProgressBar progress0, progress1, progress2;
 
-    private int progresoactual1 ;
-    private int progresoactual2 ;
-    private int progresoactual3 ;
-    
+    private int progresoactual1;
+    private int progresoactual2;
+    private int progresoactual3;
+
     private ExecutorService executor = Executors.newFixedThreadPool(3); // Creem una pool de dos fils
-    private Future<?> tarea1 ,tarea2,tarea3;
+    private Future<?> tarea1, tarea2, tarea3;
     private boolean runningtarea1 = false;
     private boolean runningtarea2 = false;
     private boolean runningtarea3 = false;
-
-
-    
 
     @FXML
     private void animateToView1(ActionEvent event) {
@@ -48,51 +44,49 @@ public class Controller0 {
     @FXML
     private void runtareas(ActionEvent event) {
         Button sourceButton = (Button) event.getSource();
-        //esto es para el boton 1
-        if (sourceButton== boton1){
-            if (runningtarea1 == true){
+        // esto es para el boton 1
+        if (sourceButton == boton1) {
+            if (runningtarea1 == true) {
                 tarea1.cancel(true);
                 runningtarea1 = false;
                 boton1.setText("iniciar");
-            }else {
+            } else {
                 tarea1 = backgroundTask(0, progresoactual1);
                 runningtarea1 = true;
                 boton1.setText("Stop");
             }
-            
+
         }
-        if (sourceButton== boton2){
-            if (runningtarea2 == true){
+        if (sourceButton == boton2) {
+            if (runningtarea2 == true) {
                 tarea2.cancel(true);
                 runningtarea2 = false;
                 boton2.setText("iniciar");
-            }else {
+            } else {
                 tarea2 = backgroundTask(1, progresoactual2);
                 runningtarea2 = true;
                 boton2.setText("Stop");
             }
-            
+
         }
-        if (sourceButton== boton3){
-            if (runningtarea3 == true){
+        if (sourceButton == boton3) {
+            if (runningtarea3 == true) {
                 tarea3.cancel(true);
                 runningtarea3 = false;
                 boton3.setText("iniciar");
-            }else {
+            } else {
                 tarea3 = backgroundTask(2, progresoactual3);
                 runningtarea3 = true;
                 boton3.setText("Stop");
             }
-            
+
         }
-        
+
     }
 
-
     private Future<?> backgroundTask(int index, int numprogreso) {
-        // Executar la tasca
         final int finalvalor = numprogreso;
-        
+
         return executor.submit(() -> {
             int valor = finalvalor;
             Random random = new Random();
@@ -105,52 +99,49 @@ public class Controller0 {
                     valor = 100;
                 }
                 final int currentValue;
-                if (valor < 100){
+                if (valor < 100) {
                     currentValue = valor;
-                    
-                }else {
+
+                } else {
                     currentValue = 0;
-                   
+
                 }
                 if (index == 0) {
-                    // Actualitzar el Label en el fil d'aplicació de l'UI
-                    
+
                     Platform.runLater(() -> {
-                        progresoactual1=currentValue;
-                        percentatge0.setText(String.valueOf(currentValue) + "%");                            
-                        progress0.setProgress(currentValue/100.0);
-                        progresoactual1=currentValue;
-              
+                        progresoactual1 = currentValue;
+                        percentatge0.setText(String.valueOf(currentValue) + "%");
+                        progress0.setProgress(currentValue / 100.0);
+                        progresoactual1 = currentValue;
+
                         if (progresoactual1 == 99) {
                             progresoactual1 = 0;
-                            
+
                             boton1.setText("Activar");
                         }
-                        
+
                     });
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
-                        // Vuelve a establecer la bandera de interrupción
                         Thread.currentThread().interrupt();
                     }
 
                 }
-                //linea 2
+                // linea 2
                 if (index == 1) {
-                    // Actualitzar el Label en el fil d'aplicació de l'UI
-                    
-                    int randomNumber = random.nextInt(3) + 2; 
+
+                    int randomNumber = random.nextInt(3) + 2;
                     int numeroAleatorio = (random.nextInt(3) + 3);
-                    numeroAleatorio = numeroAleatorio*1000;
-                    
-                    int numero = valor+randomNumber;
-                    
+                    numeroAleatorio = numeroAleatorio * 1000;
+
+                    int numero = valor + randomNumber;
+
                     Platform.runLater(() -> {
-                        progresoactual2=currentValue;
+                        progresoactual2 = currentValue;
                         percentatge1.setText(String.valueOf(numero) + "%");
-                        progress1.setProgress(numero/100.0);
-                        if (progresoactual2 == 99){
+                        progress1.setProgress(numero / 100.0);
+                        if (progresoactual2 == 99) {
                             stopExecutor();
                             boton2.setText("Activar");
                         }
@@ -159,29 +150,27 @@ public class Controller0 {
                     try {
                         Thread.sleep(numeroAleatorio);
                     } catch (InterruptedException e) {
-                        // Vuelve a establecer la bandera de interrupción
                         Thread.currentThread().interrupt();
                     }
-                    
+
                 }
 
                 if (index == 2) {
                     int randomNumber = random.nextInt(3) + 4;
-                    int numero = currentValue+randomNumber;
-                    final int finalnumero = numero+randomNumber;
-                    System.err.println("current value = "+currentValue);
-                    System.out.println("aleatorio= "+randomNumber);
-                    System.out.println("resultado =" +numero);
+                    int numero = currentValue + randomNumber;
+                    final int finalnumero = numero + randomNumber;
+                    System.err.println("current value = " + currentValue);
+                    System.out.println("aleatorio= " + randomNumber);
+                    System.out.println("resultado =" + numero);
 
                     int numeroAleatorio = random.nextInt(6) * 1000 + 3000;
-                    // Actualitzar el Label en el fil d'aplicació de l'UI
                     Platform.runLater(() -> {
-                        
-                        progresoactual3=currentValue;
-                        
+
+                        progresoactual3 = currentValue;
+
                         percentatge2.setText(String.valueOf(finalnumero) + "%");
-                        progress2.setProgress(numero/100.0);
-                        if (progresoactual3 == 99){
+                        progress2.setProgress(numero / 100.0);
+                        if (progresoactual3 == 99) {
                             stopExecutor();
                             boton3.setText("Activar");
                         }
@@ -189,20 +178,18 @@ public class Controller0 {
                     try {
                         Thread.sleep(numeroAleatorio);
                     } catch (InterruptedException e) {
-                        // Vuelve a establecer la bandera de interrupción
                         Thread.currentThread().interrupt();
                     }
-                    
+
                 }
 
-                //System.out.println("Updating label: " + index + ", Value: " + currentValue);
+                // System.out.println("Updating label: " + index + ", Value: " + currentValue);
             }
         });
     }
 
-    
-    
-    // Aquesta funció la cridaries quan vulguis tancar l'executor (per exemple, quan tanquis la teva aplicació)
+    // Aquesta funció la cridaries quan vulguis tancar l'executor (per exemple, quan
+    // tanquis la teva aplicació)
     public void stopExecutor() {
         executor.shutdown();
     }
--- src/main/java/com/project/Controller1.java
+++ src/main/java/com/project/Controller1.java
@@ -18,67 +18,64 @@ import java.util.function.Consumer;
 public class Controller1 implements initialize {
 
     @FXML
-    private Button button0, button1,stop;
+    private Button button0, button1, stop;
     @FXML
-    private ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9,img10
-    ,img11,img12,img13,img14,img15,img16,img17,img18,img19,img20,img21,img22,img23,img24;
+    private ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15,
+            img16, img17, img18, img19, img20, img21, img22, img23, img24;
     @FXML
     private Label progreso;
     @FXML
     private ProgressBar progressBar;
-    
+
     @FXML
     private AnchorPane container;
-    
-    
 
     private List<String> imageUrls = List.of(
-        "/assets/image1.png", "/assets/image2.png", "/assets/image3.png",
-        "/assets/image4.png", "/assets/image5.png", "/assets/image6.png",
-        "/assets/image7.png", "/assets/image8.png", "/assets/image9.png",
-        "/assets/image10.png", "/assets/image11.png", "/assets/image12.png",
-        "/assets/image13.png", "/assets/image14.png", "/assets/image15.png",
-        "/assets/image16.png", "/assets/image17.png", "/assets/image18.png",
-        "/assets/image19.png", "/assets/image20.png", "/assets/image21.png",
-        "/assets/image22.png", "/assets/image23.png", "/assets/image24.png");    
+            "/assets/image1.png", "/assets/image2.png", "/assets/image3.png",
+            "/assets/image4.png", "/assets/image5.png", "/assets/image6.png",
+            "/assets/image7.png", "/assets/image8.png", "/assets/image9.png",
+            "/assets/image10.png", "/assets/image11.png", "/assets/image12.png",
+            "/assets/image13.png", "/assets/image14.png", "/assets/image15.png",
+            "/assets/image16.png", "/assets/image17.png", "/assets/image18.png",
+            "/assets/image19.png", "/assets/image20.png", "/assets/image21.png",
+            "/assets/image22.png", "/assets/image23.png", "/assets/image24.png");
     private int currentImageIndex = 0;
     private int contadorProgreso = 0;
     private volatile boolean stopThread = false;
     private volatile boolean stopProgressUpdate = false;
- 
 
     @FXML
     private void animateToView0(ActionEvent event) {
         UtilsViews.setViewAnimating("View0");
     }
+
     @FXML
     private void stopExecutor(ActionEvent event) {
         stopThread = true;
         stopProgressUpdate = true;
         clearImageViews();
-        
+
     }
 
     @FXML
     private void loadImage() {
-    System.out.println("Loading images...");
+        System.out.println("Loading images...");
 
-    img1.setImage(null);
-    loadNextImage();
-}
+        img1.setImage(null);
+        loadNextImage();
+    }
 
     private void loadNextImage() {
         if (currentImageIndex < imageUrls.size()) {
             String imageUrl = imageUrls.get(currentImageIndex);
             ImageView targetImageView = getImageViewById(currentImageIndex);
-            
-    
+
             loadImageBackground(imageUrl, (image) -> {
                 System.out.println("Image loaded");
                 targetImageView.setImage(image);
-                
+
                 contadorProgreso++;
-                
+
                 System.out.println(contadorProgreso);
                 if (!stopProgressUpdate) {
                     Platform.runLater(() -> {
@@ -88,11 +85,11 @@ public class Controller1 implements initialize {
                 }
                 currentImageIndex++;
                 loadNextImage();
-                
+
             });
         }
     }
-    
+
     private ImageView getImageViewById(int index) {
         return switch (index) {
             case 0 -> img1;
@@ -123,22 +120,20 @@ public class Controller1 implements initialize {
         };
     }
 
-
     public void loadImageBackground(String imageUrl, Consumer<Image> callBack) {
-        // Use a thread to avoid blocking the UI
+
         CompletableFuture<Image> futureImage = CompletableFuture.supplyAsync(() -> {
             if (stopThread) {
-                return null; // Salir temprano si se debe detener el hilo
+                return null;
             }
             try {
-                // Simulate a loading time
+
                 Random random = new Random();
                 int numeroAleatorio = random.nextInt(46) + 5;
-                numeroAleatorio = numeroAleatorio*1000;
+                numeroAleatorio = numeroAleatorio * 1000;
                 System.out.println(numeroAleatorio);
                 Thread.sleep(numeroAleatorio);
 
-                // Load the data from the assets file
                 Image image = new Image(getClass().getResource(imageUrl).toString());
                 return image;
             } catch (InterruptedException e) {
@@ -146,20 +141,22 @@ public class Controller1 implements initialize {
                 return null;
             }
         })
-        .exceptionally(ex -> {
-            ex.printStackTrace();
-            return null;
-        });
+                .exceptionally(ex -> {
+                    ex.printStackTrace();
+                    return null;
+                });
 
         futureImage.thenAcceptAsync(result -> {
-        // Verificar nuevamente si se debe detener antes de mostrar la imagen
-        if (!stopThread) {
-            callBack.accept(result);
-        }
-    }, Platform::runLater);
+
+            if (!stopThread) {
+                callBack.accept(result);
+            }
+        }, Platform::runLater);
     }
+
     private void clearImageViews() {
-        ImageView[] imageViews = {img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24};
+        ImageView[] imageViews = { img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13,
+                img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24 };
         for (ImageView imageView : imageViews) {
             imageView.setImage(null);
         }
